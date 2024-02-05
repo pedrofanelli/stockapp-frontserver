147779531353f83c79a3d058f508f9d099288c6d
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-02-05 20:04:04 -0300

    init

---
+++ src/main/java/com/stockapp/frontserver/StockappFrontserverApplication.java
@@ -0,0 +1,13 @@
+package com.stockapp.frontserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockappFrontserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockappFrontserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockapp/frontserver/StockappFrontserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.frontserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockappFrontserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
